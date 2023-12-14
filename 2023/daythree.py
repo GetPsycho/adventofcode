721c756e79ad4fc0a24cc9e66f05ade02b8e14c0
GetPsycho <38526863+GetPsycho@users.noreply.github.com>
2023-12-15 00:47:27 +0100

    part1 completed

--- 2023/daythree.py
+++ 2023/daythree.py
@@ -1,85 +1,176 @@
 
 
-
+import os
 import re
-f = open(r"C:\Users\erifontaine\repopo\adventofcode\2023\input_daythree.txt", "r").read().strip()
+
+def check_in_range(array, index):
+    return 0 <= index < len(array)
+
+work_folder = os.getcwd()
+input_relative_path = "2023/input_daythree.txt"
+input_absolute_path = os.path.join(work_folder,input_relative_path)
+f = open(input_absolute_path, "r").read().strip()
 #determiner le nb de lignes et colonnes du tableau à remplir
+
+
 splitFile = f.split("\n")
 lineNumber = len(splitFile)
 StringLength = len(splitFile[0])
+globalCount = 0
 
-array2Dempty = [["."] * StringLength for _ in range(lineNumber)]
+#create the empty 2d array
+array2D = [["."] * StringLength for _ in range(lineNumber)]
 
+#populate the 2d array
 for indiceLine, elemLine in enumerate(f.split("\n")):
     for indiceCol, elemChar in enumerate(elemLine):
-        print(indiceCol)
-        print(elemChar)
-        array2Dempty[indiceLine][indiceCol] = elemChar
+        array2D[indiceLine][indiceCol] = elemChar
 
-print(array2Dempty)
+# print(array2D)
 
-for arIndiceLine, arLine in enumerate(array2Dempty):
-    for arIndiceChar, arCharELem in enumerate(elemLine):
+# PART ONE #############################################################################################
+# #browse to find valid parameters
+# for arIndiceLine, arLine in enumerate(array2D):
+#     print("arLine: "+ str(arLine))
+#     for arIndiceChar, arCharELem in enumerate(arLine):
+#         print("arIndiceChar: "+ str(arIndiceChar))
+#         print("arCharELem: "+ str(arCharELem))
+#         potential_parameter = ""
+#         if(arCharELem.isdigit()):
+#             candidateNb = str(arCharELem)
+#             currentPosition = arIndiceChar
+            
+#             #determine if next character is a digit + create the parameter string
+#             while(candidateNb.isdigit()):
+#                 potential_parameter += candidateNb
+#                 currentPosition += 1
+#                 if(check_in_range(array2D[arIndiceLine], currentPosition)):
+#                     candidateNb = str(array2D[arIndiceLine][currentPosition])
+#                 else:
+#                     break
+#         if len(potential_parameter) != 0:
+#             #Check that there is special chars around the parameter found
+#             print("potential_parameter: " + str(potential_parameter))
+#             adjacent_number_found = False
+#             valid_parameter = False
+#             #before and after
+#             if(check_in_range(array2D[arIndiceLine], arIndiceChar-1)):
+#                 before_char = str(array2D[arIndiceLine][arIndiceChar-1])
+#                 print("just before:" + str(before_char))
+#                 if( str(before_char)!= "." and not str(before_char).isdigit()):
+#                     valid_parameter = True
+#                 if(str(before_char).isdigit()):
+#                     adjacent_number_found = True
+#             if(check_in_range(array2D[arIndiceLine], currentPosition)):
+#                 after_char = str(array2D[arIndiceLine][currentPosition])
+#                 print("just after:" + str(after_char))
+#                 if(after_char!= "." and not str(after_char).isdigit()):
+#                     valid_parameter = True
+#                 if(str(after_char).isdigit()):
+#                     adjacent_number_found = True
+
+            
+#             #above and below
+#             iteration_for_above = currentPosition - arIndiceChar + 2
+#             for i in range(iteration_for_above):
+#                 if(check_in_range(array2D, arIndiceLine-1) and check_in_range(array2D[arIndiceLine-1], arIndiceChar-1+i)):
+#                     above_char = str(array2D[arIndiceLine-1][arIndiceChar-1+i])
+#                     print("above:" + above_char)
+#                     if( above_char!= "." and not above_char.isdigit()):
+#                         valid_parameter = True
+#                         break
+#                 if(check_in_range(array2D, arIndiceLine+1) and check_in_range(array2D[arIndiceLine+1], arIndiceChar-1+i)):
+#                     below_char = str(array2D[arIndiceLine+1][arIndiceChar-1+i])
+#                     print("below: " + below_char)
+#                     if(below_char!= "." and not below_char.isdigit()):
+#                         valid_parameter = True
+#                         break
+#             print("valid_parameter:" + str(valid_parameter))
+#             print("adjacent_number_found:" + str(adjacent_number_found))
+#             print("potential_parameter:" + str(potential_parameter))
+#             if(valid_parameter and not adjacent_number_found):
+#                 globalCount += int(potential_parameter)
+#                 print("***********globalCount: " + str(globalCount))
+#                 print("arIndiceLine: "+ str(arIndiceLine))
+#                 print("***********parametre ajouté: "+ str(potential_parameter))
+#                 print("***********")
+#             else:
+#                 print("***********parametre PAS ajouté: "+ str(potential_parameter))
+#     print("boucle:"+str(arIndiceLine)+" terminée")
+
+# print("globalCount: "+ str(globalCount))
+
+
+# PART TWO #############################################################################################
+#browse to find valid parameters
+for arIndiceLine, arLine in enumerate(array2D):
+    print("arLine: "+ str(arLine))
+    for arIndiceChar, arCharELem in enumerate(arLine):
+        print("arIndiceChar: "+ str(arIndiceChar))
+        print("arCharELem: "+ str(arCharELem))
+        potential_parameter = ""
         if(arCharELem.isdigit()):
-            candidateNb = arCharELem
-            while
+            candidateNb = str(arCharELem)
+            currentPosition = arIndiceChar
+            
+            #determine if next character is a digit + create the parameter string
+            while(candidateNb.isdigit()):
+                potential_parameter += candidateNb
+                currentPosition += 1
+                if(check_in_range(array2D[arIndiceLine], currentPosition)):
+                    candidateNb = str(array2D[arIndiceLine][currentPosition])
+                else:
+                    break
+        if len(potential_parameter) != 0:
+            #Check that there is special chars around the parameter found
+            print("potential_parameter: " + str(potential_parameter))
+            adjacent_number_found = False
+            valid_parameter = False
+            #before and after
+            if(check_in_range(array2D[arIndiceLine], arIndiceChar-1)):
+                before_char = str(array2D[arIndiceLine][arIndiceChar-1])
+                print("just before:" + str(before_char))
+                if( str(before_char)!= "." and not str(before_char).isdigit()):
+                    valid_parameter = True
+                if(str(before_char).isdigit()):
+                    adjacent_number_found = True
+            if(check_in_range(array2D[arIndiceLine], currentPosition)):
+                after_char = str(array2D[arIndiceLine][currentPosition])
+                print("just after:" + str(after_char))
+                if(after_char!= "." and not str(after_char).isdigit()):
+                    valid_parameter = True
+                if(str(after_char).isdigit()):
+                    adjacent_number_found = True
 
+            
+            #above and below
+            iteration_for_above = currentPosition - arIndiceChar + 2
+            for i in range(iteration_for_above):
+                if(check_in_range(array2D, arIndiceLine-1) and check_in_range(array2D[arIndiceLine-1], arIndiceChar-1+i)):
+                    above_char = str(array2D[arIndiceLine-1][arIndiceChar-1+i])
+                    print("above:" + above_char)
+                    if( above_char!= "." and not above_char.isdigit()):
+                        valid_parameter = True
+                        break
+                if(check_in_range(array2D, arIndiceLine+1) and check_in_range(array2D[arIndiceLine+1], arIndiceChar-1+i)):
+                    below_char = str(array2D[arIndiceLine+1][arIndiceChar-1+i])
+                    print("below: " + below_char)
+                    if(below_char!= "." and not below_char.isdigit()):
+                        valid_parameter = True
+                        break
+            print("valid_parameter:" + str(valid_parameter))
+            print("adjacent_number_found:" + str(adjacent_number_found))
+            print("potential_parameter:" + str(potential_parameter))
+            if(valid_parameter and not adjacent_number_found):
+                globalCount += int(potential_parameter)
+                print("***********globalCount: " + str(globalCount))
+                print("arIndiceLine: "+ str(arIndiceLine))
+                print("***********parametre ajouté: "+ str(potential_parameter))
+                print("***********")
+            else:
+                print("***********parametre PAS ajouté: "+ str(potential_parameter))
+    print("boucle:"+str(arIndiceLine)+" terminée")
 
-##############################################Daytwo - part#1 ############################################################
-# stringExample = "Game 1: 1 red, 3 blue, 11 green; 1 blue, 5 red; 3 blue, 5 green, 13 red; 6 red, 1 blue, 4 green; 16 red, 12 green"
-# globalCount = 0
-# for calibElement in f.split('\n'):
-#     splittedList = calibElement.split(":")
-#     gameId = int(splittedList[0].split(" ")[-1])
-#     validGame = True
-#     revealList = splittedList[-1]
-#     CubesFromReveal = re.split(r'[;,]',revealList)
-#     for cubes in CubesFromReveal:
-#         splittedCube = cubes.strip().split(" ")
-#         if (splittedCube[-1] == "red" and int(splittedCube[0]) > 12):
-#             validGame = False
-#             break
-#         elif (splittedCube[-1] == "green" and int(splittedCube[0]) > 13): 
-#                 validGame = False
-#                 break
-#         else:
-#             if int(splittedCube[0]) > 14: 
-#                 validGame = False
-#                 break
-#     print ("gameId: "+str(gameId)+"| Liste: "+str(CubesFromReveal)+" |validGame: "+str(validGame))
-#     if (validGame):
-#         globalCount += gameId
+print("globalCount: "+ str(globalCount))
 
-# print(globalCount)
-
-##############################################Daytwo - part#2 ############################################################
-
-# stringExampleList = (
-#     "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-#     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-#     "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-#     "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-#     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-# )
-# globalCount = 0
-# for calibElement in f.split('\n'):
-#     splittedList = calibElement.split(":")
-#     gameId = int(splittedList[0].split(" ")[-1])
-#     revealList = splittedList[-1]
-#     CubesFromReveal = re.split(r'[;,]',revealList)
-#     minRed = 0
-#     minGreen = 0
-#     minBlue = 0
-#     power = 0
-#     for cubes in CubesFromReveal:
-#         splittedCube = cubes.strip().split(" ")
-#         if (splittedCube[-1] == "red" and int(splittedCube[0]) > minRed):
-#             minRed = int(splittedCube[0])
-#         if (splittedCube[-1] == "green" and int(splittedCube[0]) > minGreen): 
-#             minGreen = int(splittedCube[0])
-#         if (splittedCube[-1] == "blue" and int(splittedCube[0]) > minBlue): 
-#             minBlue = int(splittedCube[0])
-#     power = minRed*minGreen*minBlue
-#     globalCount += power
-
-# print(globalCount)
+        
