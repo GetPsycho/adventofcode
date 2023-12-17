8a5ffdfd7504465d16fa6565cf9beeaca80389da
Get Psycho <get.psycho@gmail.com>
2023-12-18 00:10:07 +0100

    bim

--- 2023/day_04.py
+++ 2023/day_04.py
@@ -16,43 +16,45 @@ globalCount = 0
 ################################################### PART 1 ##########################################################################
 
 #we browse, line by line
-for indice_line, data_line in enumerate(f.split("\n")):
+# for indice_line, data_line in enumerate(f.split("\n")):
 
-    split_List = data_line.split(":")
-    card_name = split_List[0]
-    card_data = split_List[1]
-    card_data_split = card_data.split("|")
+#     split_List = data_line.split(":")
+#     card_name = split_List[0]
+#     card_data = split_List[1]
+#     card_data_split = card_data.split("|")
 
-    #winning numbers list
-    number_winning_list = card_data_split[0]
-    number_winning_list = number_winning_list.replace("  "," ")
-    number_winning_list_split = number_winning_list.strip().split(" ")
-    print("number_winning_list_split: "+str(number_winning_list_split))
+#     #winning numbers list
+#     number_winning_list = card_data_split[0]
+#     number_winning_list = number_winning_list.replace("  "," ")
+#     number_winning_list_split = number_winning_list.strip().split(" ")
+#     print("number_winning_list_split: "+str(number_winning_list_split))
 
-    #owned numbers list
-    number_owned_list = card_data_split[1]
-    number_owned_list = number_owned_list.replace("  "," ")
-    number_owned_list_split = number_owned_list.strip().split(" ")
-    print("number_owned_list_split: "+str(number_owned_list_split))
+#     #owned numbers list
+#     number_owned_list = card_data_split[1]
+#     number_owned_list = number_owned_list.replace("  "," ")
+#     number_owned_list_split = number_owned_list.strip().split(" ")
+#     print("number_owned_list_split: "+str(number_owned_list_split))
 
-    #we check if there is winning numbers
-    matching_numbers = 0
-    for owned_number in number_owned_list_split:
-        if owned_number in number_winning_list_split:
-            matching_numbers = matching_numbers+1
+#     #we check if there is winning numbers
+#     matching_numbers = 0
+#     for owned_number in number_owned_list_split:
+#         if owned_number in number_winning_list_split:
+#             matching_numbers = matching_numbers+1
 
-    #card point value calculation
-    # 1 = 1, 2 = 2 ,3 = 4, 4 = 8, 5 = 16, etc...
-    card_value = 1
-    if(matching_numbers != 0):
-        card_value = 2 ** (matching_numbers-1)
-        globalCount +=card_value
-    print("***********globalCount: " + str(globalCount))
+#     #card point value calculation
+#     # 1 = 1, 2 = 2 ,3 = 4, 4 = 8, 5 = 16, etc...
+#     card_value = 1
+#     if(matching_numbers != 0):
+#         card_value = 2 ** (matching_numbers-1)
+#         globalCount +=card_value
+#     print("***********globalCount: " + str(globalCount))
 
 
 
 ################################################### PART 2 ##########################################################################
-    
+
+deck_winning_cards = {}
+deck_owned_cards = {}
 #we browse, line by line
 for indice_line, data_line in enumerate(f.split("\n")):
 
@@ -61,31 +63,41 @@ for indice_line, data_line in enumerate(f.split("\n")):
     card_data = split_List[1]
     card_data_split = card_data.split("|")
 
+    card_name = card_name.split(" ")[-1]
+
     #winning numbers list
     number_winning_list = card_data_split[0]
     number_winning_list = number_winning_list.replace("  "," ")
     number_winning_list_split = number_winning_list.strip().split(" ")
-    print("number_winning_list_split: "+str(number_winning_list_split))
+    # print("number_winning_list_split: "+str(number_winning_list_split))
 
     #owned numbers list
     number_owned_list = card_data_split[1]
     number_owned_list = number_owned_list.replace("  "," ")
     number_owned_list_split = number_owned_list.strip().split(" ")
-    print("number_owned_list_split: "+str(number_owned_list_split))
+    # print("number_owned_list_split: "+str(number_owned_list_split))
+
+    deck_winning_cards[card_name]=number_winning_list_split
+    deck_owned_cards[card_name]=number_owned_list_split
+
+print('deck_winning_cards: '+ str(deck_winning_cards))
+print('deck_owned_cards: '+ str(deck_owned_cards))
+
+
 
     #we check if there is winning numbers
-    matching_numbers = 0
-    for owned_number in number_owned_list_split:
-        if owned_number in number_winning_list_split:
-            matching_numbers = matching_numbers+1
+    # matching_numbers = 0
+    # for owned_number in number_owned_list_split:
+    #     if owned_number in number_winning_list_split:
+    #         matching_numbers = matching_numbers+1
 
     #card point value calculation
     # 1 = 1, 2 = 2 ,3 = 4, 4 = 8, 5 = 16, etc...
-    card_value = 1
-    if(matching_numbers != 0):
-        card_value = 2 ** (matching_numbers-1)
-        globalCount +=card_value
-    print("***********globalCount: " + str(globalCount))
+    # card_value = 1
+    # if(matching_numbers != 0):
+    #     card_value = 2 ** (matching_numbers-1)
+    #     globalCount +=card_value
+    # print("***********globalCount: " + str(globalCount))
 
 
     #
