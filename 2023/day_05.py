a62dddd98bd116294d51871ef1710cde466bb6d3
Get Psycho <get.psycho@gmail.com>
2023-12-20 12:26:26 +0100

    files for day 5

---
+++ 2023/day_05.py
@@ -0,0 +1,7 @@
+import os
+
+#import + read the file
+work_folder = os.getcwd()
+input_relative_path = "2023/input_day_05.txt"
+input_absolute_path = os.path.join(work_folder,input_relative_path)
+f = open(input_absolute_path, "r").read().strip()
