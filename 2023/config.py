abc60b13a918b8bc6437f2de8360475af49f1b0d
Get Psycho <get.psycho@gmail.com>
2024-11-04 23:08:04 +0100

    separated conf per computer

---
+++ 2023/config.py
@@ -0,0 +1,13 @@
+import os
+import socket
+
+# Récupérer le nom de l'ordinateur
+computer_name = socket.gethostname()
+
+#import + read the file
+work_folder = os.getcwd()
+if (computer_name == 'ETL-SOP-POR0196'):
+    input_relative_path = "2023\\"
+else:
+    input_relative_path = "adventofcode\\2023\\"
+input_absolute_path = os.path.join(work_folder,input_relative_path)
--- 2023/day_05.py
+++ 2023/day_05.py
@@ -1,10 +1,7 @@
-import os
+import config
 
-#import + read the file
-work_folder = os.getcwd()
-input_relative_path = "adventofcode\\2023\\input_day_05.txt"
-input_absolute_path = os.path.join(work_folder,input_relative_path)
-f = open(input_absolute_path, "r").read().strip()
+input_file = 'input_day_05.txt'
+f = open(config.input_absolute_path+input_file, "r").read().strip()
 
 #split line by line
 for line_number, line in enumerate(f.split("\n")):
@@ -12,9 +9,12 @@ for line_number, line in enumerate(f.split("\n")):
     #put the seeds into a data structure
     # print('line_number: '+ str(line_number)+ ' line:'+ str(line))
     if line.startswith('seeds'):
-        split_seed_line = line.split(':')
-        print('line: '+ str(split_seed_line))
-
+        split_seed_id = line.split(':')[1]
+        print('seeds id: '+ str(split_seed_id))
+    # for seed_id in split_seed_id.split():
+    #     print('seed_id: '+ seed_id)
+    if line.endswith('map:'):
+        
 
 
 
