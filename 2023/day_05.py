4338f3066633773268be8b5852ca32ea3c3299cc
Get Psycho <get.psycho@gmail.com>
2024-11-04 09:58:37 +0100

    bim les derniers commits

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -5,3 +5,5 @@ work_folder = os.getcwd()
 input_relative_path = "2023/input_day_05.txt"
 input_absolute_path = os.path.join(work_folder,input_relative_path)
 f = open(input_absolute_path, "r").read().strip()
+
+
